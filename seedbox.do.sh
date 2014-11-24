75e746f4255fe19d754a6db5faf28eb9676423b9
Matt Condon <mattgcondon@gmail.com>
2014-11-24 00:24:50 -0600

    update

--- seedbox.do.sh
+++ seedbox.do.sh
@@ -2,10 +2,11 @@
 
 # Script to set up a basid seedbox on a Ubuntu 14.04 VPS
 
-# ask for username/password
-# ask for transmission user/password
+# ask for username/password for everything
+read -e -p "Choose Username: " sb_username
 
 # create user
+useradd -m $sb_username -s /bin/bash -G sudo -U -d/home/$sb_username
 # lock down root ssh
 
 # install openvpn and set up
