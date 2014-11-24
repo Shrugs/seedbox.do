b63ecb2548695d160ae21c037b603e4813ce884a
Matt Condon <mattgcondon@gmail.com>
2014-11-24 00:20:02 -0600

    init commit

---
+++ seedbox.do.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+# Script to set up a basid seedbox on a Ubuntu 14.04 VPS
+
+# ask for username/password
+# ask for transmission user/password
+
+# create user
+# lock down root ssh
+
+# install openvpn and set up
+
+# install transmission, change password to variable, restart and serve
+# set up directory structure
+
+
+# install plex, give user url to configure
+
+
+# ssh-copy-id ~/.ssh/id_rsa.pub viking@<ip>
